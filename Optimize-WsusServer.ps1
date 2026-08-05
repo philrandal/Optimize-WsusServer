3747ca4a95241a00a639a0d2611d743276e5a16c
Phil Randal <phil.randal@gmail.com>
2026-08-05 15:06:28 +0100

    Update Optimize-WsusServer.ps1

--- Optimize-WsusServer.ps1
+++ Optimize-WsusServer.ps1
@@ -130,7 +130,7 @@ $unneededUpdatesbyProductTitles = @(
     "Windows Server 2008 R2",
     "Windows Server 2008",
 	"Windows Server 2012 R2",
-	"Windows Servwe 2012",
+	"Windows Server 2012",
     "Windows Ultimate Extras",
     "Windows Vista",
     "Windows XP Embedded",
